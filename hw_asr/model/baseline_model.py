b59a1a5bf692ced8093217b43e14f1d16d43a637
Alexandra Kogan <yarolika00@mail.ru>
2021-10-16 22:54:36 +0300

    Add files via upload

--- hw_asr/model/baseline_model.py
+++ hw_asr/model/baseline_model.py
@@ -7,17 +7,22 @@ from hw_asr.base import BaseModel
 class BaselineModel(BaseModel):
     def __init__(self, n_feats, n_class, fc_hidden=512, *args, **kwargs):
         super().__init__(n_feats, n_class, *args, **kwargs)
-        self.net = Sequential(
+        #self.net = Sequential(
             # people say it can aproximate any function...
-            nn.Linear(in_features=n_feats, out_features=fc_hidden),
-            nn.ReLU(),
-            nn.Linear(in_features=fc_hidden, out_features=fc_hidden),
-            nn.ReLU(),
-            nn.Linear(in_features=fc_hidden, out_features=n_class)
-        )
+         #   nn.Linear(in_features=n_feats, out_features=fc_hidden),
+         #   nn.ReLU(),
+         #   nn.Linear(in_features=fc_hidden, out_features=fc_hidden),
+          #  nn.ReLU(),
+           # nn.Linear(in_features=fc_hidden, out_features=n_class)
+        #)
+        self.rnn = nn.RNN(input_size = n_feats, hidden_size=128, num_layers=6)
+        self.out = nn.Linear(in_features=128, out_features=n_class)
+
+
 
     def forward(self, spectrogram, *args, **kwargs):
-        return {"logits": self.net(spectrogram)}
+        output, _ = self.rnn(spectrogram)
+        return {"logits": self.out(output)}
 
     def transform_input_lengths(self, input_lengths):
         return input_lengths  # we don't reduce time dimension here
